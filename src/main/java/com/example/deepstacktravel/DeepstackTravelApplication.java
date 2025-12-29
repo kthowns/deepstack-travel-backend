b62ba538710f31fbd45a4bca339cfbf5fa480d32
KTH <pni2396@gmail.com>
2025-12-29 14:49:33 +0900

    setting: project init

---
+++ src/main/java/com/example/deepstacktravel/DeepstackTravelApplication.java
@@ -0,0 +1,13 @@
+package com.example.deepstacktravel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeepstackTravelApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DeepstackTravelApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/deepstacktravel/DeepstackTravelApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.deepstacktravel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeepstackTravelApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
